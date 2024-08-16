460e6827bf6747dc76b6c5b93fd417c3287f5ec1
Igor Todorovski <itodorov@ca.ibm.com>
2024-08-16 13:54:25 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/cjsonport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/cjsonport/actions/workflows/bump.yml)
+
+# cjson
+
+Ultralightweight JSON parser in ANSI C
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cjsonport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cjsonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ultralightweight JSON parser in ANSI C' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
