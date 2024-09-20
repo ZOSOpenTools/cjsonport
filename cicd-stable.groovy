b1a6c4c6ffc2ff7dce492277bdf2db8104d94790
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:38:00 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/cjsonport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/cjsonport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/cjsonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ultralightweight JSON parser in ANSI C' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/cjsonport.git'), string(name: 'PORT_DESCRIPTION', value: 'Ultralightweight JSON parser in ANSI C' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
